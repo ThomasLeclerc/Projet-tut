b3154de6b401f7116bd64c2636d3049c55f44403
Thomas <thomas@thomas-GPC.(none)>
2012-09-06 15:13:45 +0200

    séparation de la classe Ship

---
+++ src/Ship.py
@@ -0,0 +1,51 @@
+import pygame, sys
+
+class ship:
+    monte = False
+    desc = False
+    posX = 20
+    posY = 0
+    speed=0
+    switch=0
+    def getPos(self):
+        return (self.posX,self.posY)
+    def setImg(self, file):
+        self.img = pygame.image.load(file)
+        self.shiprect = self.img.get_rect()
+
+    #
+    # deplace le vaisseau,   
+    # prend en param les 2 fichiers reacteurs alumes
+    #
+    def bouge(self,file0,file1):
+        accel=0.8
+        if self.monte == True:
+            if self.posY-2 >= 0:
+                self.posY+=self.speed
+                self.speed-=accel
+            else:
+                self.speed = 0
+            #animation    
+            if self.switch == 0:
+                self.setImg(file0)
+                self.switch=1
+            elif self.switch == 1:
+                self.setImg(file1)
+                self.switch=0
+        else:
+            if self.posY+2 <= height-(self.shiprect).bottom:
+                self.posY+=self.speed
+                self.speed+=accel
+            else:
+                self.speed = 0
+                
+        #blockage du vaisseau dans la fenetre
+        if self.posY < 0:
+            self.posY = 0
+            self.speed = 0
+        elif self.posY > height-(self.shiprect).bottom:
+            self.posY = height-(self.shiprect).bottom
+            self.speed = 0
+    #fin fonction bouge()
+#fin classe ship
+
--- src/game.py
+++ src/game.py
@@ -1,59 +1,8 @@
 import pygame, sys
-import Obstacle, Shot
+import Obstacle, Shot, Ship
 pygame.init()
 
 
-
-
-
-class ship:
-    monte = False
-    desc = False
-    posX = 20
-    posY = 0
-    speed=0
-    switch=0
-    def getPos(self):
-        return (self.posX,self.posY)
-    def setImg(self, file):
-        self.img = pygame.image.load(file)
-        self.shiprect = self.img.get_rect()
-
-    #
-    # deplace le vaisseau,   
-    # prend en param les 2 fichiers reacteurs alumes
-    #
-    def bouge(self,file0,file1):
-        accel=0.8
-        if self.monte == True:
-            if self.posY-2 >= 0:
-                self.posY+=self.speed
-                self.speed-=accel
-            else:
-                self.speed = 0
-            #animation    
-            if self.switch == 0:
-                self.setImg(file0)
-                self.switch=1
-            elif self.switch == 1:
-                self.setImg(file1)
-                self.switch=0
-        else:
-            if self.posY+2 <= height-(self.shiprect).bottom:
-                self.posY+=self.speed
-                self.speed+=accel
-            else:
-                self.speed = 0
-                
-        #blockage du vaisseau dans la fenetre
-        if self.posY < 0:
-            self.posY = 0
-            self.speed = 0
-        elif self.posY > height-(self.shiprect).bottom:
-            self.posY = height-(self.shiprect).bottom
-            self.speed = 0
-    #fin fonction bouge()
-#fin classe ship
 
 size = width, height = 640, 400
 screen = pygame.display.set_mode(size)
@@ -68,7 +17,7 @@ stones = pygame.image.load("stones.png")
 i=j=k=t=0
 
 
-monVaisseau = ship()
+monVaisseau = Ship.ship()
 #monVaisseau.setImg("ship.png")
 #monVaisseau.setImg("chasseur.png")
 monVaisseau.setImg("pinkship.png")
