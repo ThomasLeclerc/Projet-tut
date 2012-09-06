1c4c4e65c2b46c0dd970453aa68343216374a334
Thomas <thomas@thomas-GPC.(none)>
2012-09-06 15:40:11 +0200

    début classe player

---
+++ src/Player.py
@@ -0,0 +1,16 @@
+import pygame, sys
+
+class player:
+    best_score = getBestScore()
+	score = 0
+    
+	def augmenterScore(nombre):
+		scorre += nombre
+
+	def beatBestScore():
+		if score > best_score:
+			# afficher
+
+	def getBestScore():
+		#TODO
+		
