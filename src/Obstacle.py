af8286c208b16e46bd512f31dbab14bb11fb01c7
Thomas <thomas@thomas-GPC.(none)>
2012-09-10 20:57:51 +0200

    déplacement en serpent

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -7,8 +7,10 @@ class obstacle:
     posX = 0
     posY = 0
     coef = 0
-    s = False
-    img = pygame.image.load("cible.png")
+    angleRotation = 160 #true si on monte et false si on descend
+    versLeHaut = True
+    img = pygame.image.load("./ship1.png")
+    img = pygame.transform.rotate(img,angleRotation)
     obrect = img.get_rect()
     def __init__(self,x,y):
         self.posX = x
@@ -18,19 +20,59 @@ class obstacle:
             return True 
         else:
             return False
-	
+    
     def move(self, previousObstacle):
         (poPosX,_) = previousObstacle.getPos()
-        if self.posX-poPosX >= 70:
-            self.coef += 0.1
-            self.posX -= 3	
-            self.posY += (sin(self.coef)*12)
-		
-    def moveFirst(self):	
-		self.coef += 0.1
-		self.posX -= 3
-		self.posY += (sin(self.coef)*12)
-	
+        if self.posX-poPosX >= 50:
+            self.posX -= 3    
+            self.coef += 0.12
+            self.posY += (sin(self.coef)*17)
+            
+            
+            self.img = pygame.image.load("./ship1.png")
+            if self.angleRotation == 20:
+                self.versLeHaut = False
+                self.angleRotation +=5
+                self.img = pygame.transform.rotate(self.img,self.angleRotation)                               
+            elif self.angleRotation == 160:
+                self.versLeHaut = True
+                self.angleRotation -= 5
+                self.img = pygame.transform.rotate(self.img,self.angleRotation)
+            else:
+                if self.versLeHaut == True:
+                    self.angleRotation -= 5
+                    self.img = pygame.transform.rotate(self.img,self.angleRotation)
+                else :
+                    self.angleRotation += 5
+                    self.img = pygame.transform.rotate(self.img,self.angleRotation)  
+                          
+                
+            
+
+        
+    def moveFirst(self):    
+        self.coef += 0.12
+        self.posX -= 3
+        self.posY += (sin(self.coef)*17)
+        self.img = pygame.image.load("./ship1.png")
+        if self.angleRotation == 20:
+            self.versLeHaut = False
+            self.angleRotation +=5
+            self.img = pygame.transform.rotate(self.img,self.angleRotation)                               
+        elif self.angleRotation == 160:
+            self.versLeHaut = True
+            self.angleRotation -= 5
+            self.img = pygame.transform.rotate(self.img,self.angleRotation)
+        else:
+           if self.versLeHaut == True:
+                self.angleRotation -= 5
+                self.img = pygame.transform.rotate(self.img,self.angleRotation)
+           else :
+               self.angleRotation += 5
+               self.img = pygame.transform.rotate(self.img,self.angleRotation)  
+        
+        
+    
     def getPos(self):
         return (self.posX,self.posY)
 
