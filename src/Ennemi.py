7fb4860e6fedc129c9f79da045b5fbb41cbab3af
kosinus <charlelie.petracca@gmail.com>
2012-09-10 22:06:02 +0300

    Update src/Ennemi.py
    creation classe ennemy

--- src/Ennemi.py
+++ src/Ennemi.py
@@ -1,3 +1,17 @@
-import Game.py
+import pygame
+import sys
 
-class Ennemi:
+class ennemy:
+    posX = 0
+    posY = 0
+    
+    def __init__(self,x,y):
+        self.posX = x
+        self.posY = y
+
+    def getPos(self):
+        return (self.posX,self.posY)
+    
+    def setImg(self, file):
+        self.img = pygame.image.load(file)
+        self.shiprect = self.img.get_rect()
