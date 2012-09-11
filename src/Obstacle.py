03200146eab36b35bee67464508b283bd550fa0d
Petracca <petracca@petracca-desktop.(none)>
2012-09-11 20:57:09 +0200

    creation de la classe

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -3,8 +3,17 @@ from math import sin
 
 
 class obstacle:
+    posX = 0
+    posY = 0
+    
+    def __init__(self,x,y,image):
+        self.posX = x
+        self.posY = y
+        self.setImg(image);
 
-    i=0
-
-
-
+    def getPos(self):
+        return (self.posX,self.posY)
+    
+    def setImg(self, image):
+        self.img = pygame.image.load(file)
+        self.ennemyRect = self.img.get_rect()
