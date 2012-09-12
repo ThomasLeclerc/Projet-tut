f55756db661c7005d8529c314b6e6f77c7738c0a
thomasleclerc <leclercthomas@yahoo.fr>
2012-09-12 18:53:08 +0200

    nombre de snake créer relatif a la chaleur max

--- src/game.py
+++ src/game.py
@@ -13,11 +13,9 @@ import Player
 import Obstacle
 
 def creerSnakes(nombre):
-    i = 0
     while(nombre!=0):
-        snakes.append(Ennemi.Snake(width-(i*20),80))
+        snakes.append(Ennemi.Snake(width+(nombre*20),80))
         nombre -= 1
-        i += 1
 
 pygame.init()
 
@@ -46,7 +44,7 @@ snakes = []
 ennemy = []
 obstacles = []
 ''''''
-creerSnakes(4)
+creerSnakes(int(monVaisseau.chaleurMax/33))
 ''''''
 ##### OBSTACLES #####
 #obstacles.append(Obstacle(0,"images/meteorite.png"))
@@ -162,6 +160,6 @@ while 1:
         screen.blit(img,(10*(l+2),10))
     
     if len(snakes)==0:
-        creerSnakes(4)
+        creerSnakes(int(monVaisseau.chaleurMax/33))
 
     pygame.display.flip()
